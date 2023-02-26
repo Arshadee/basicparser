9f6ded68432dfc12c10c08e8922efe305a5ad48a
Arshadee <arshadee@gmail.com>
2023-02-26 14:27:18 +0400

    added interace for tdataobject classes for more abstraction

--- src/main/java/io/basicparser/dataobjects/NodeObject.java
+++ src/main/java/io/basicparser/dataobjects/NodeObject.java
@@ -3,33 +3,38 @@ package io.basicparser.dataobjects;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.basicparser.dataobjectsInterfaces.INode;
+import io.basicparser.dataobjectsInterfaces.INodeObject;
+
 /**
  * Model class - specifically used to store node relation data parsed from string expression
  * @author Arshad
  */
-public class NodeObject {
-    private Node<String> objectElement = new Node<>();
-    private List<Node<String>> objectElements = new ArrayList<>();
+public class NodeObject implements INodeObject{
+    private INode<String> objectElement = new Node<>();
+    private List<INode<String>> objectElements = new ArrayList<>();
     
     public NodeObject() {
     }
 
-    public Node<String> getObjectElement() {
+    @Override
+    public INode<String> getObjectElement() {
         return objectElement;
     }
 
-    public void setObjectElement(Node<String> objectElement) {
+    @Override
+    public void setObjectElement(INode<String> objectElement) {
         this.objectElement = objectElement;
     }
 
-    public List<Node<String>> getObjectElements() {
+    @Override
+    public List<INode<String>> getObjectElements() {
         return objectElements;
     }
 
-    public void setObjectElements(List<Node<String>> objectElements) {
+    @Override
+    public void setObjectElements(List<INode<String>> objectElements) {
         this.objectElements = objectElements;
     }
-   
-   
    
 }
--- src/main/java/io/basicparser/dataobjectsInterfaces/INodeObject.java
+++ src/main/java/io/basicparser/dataobjectsInterfaces/INodeObject.java
@@ -1,5 +1,16 @@
 package io.basicparser.dataobjectsInterfaces;
 
+import java.util.List;
+
+import io.basicparser.dataobjects.Node;
+
 public interface INodeObject {
 
+	public INode<String> getObjectElement();
+
+	public void setObjectElement(INode<String> objectElement);
+
+	public List<INode<String>> getObjectElements();
+
+	public void setObjectElements(List<INode<String>> objectElements);
 }
